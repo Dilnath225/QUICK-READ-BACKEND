9ca2ff0eff6bbd7b2c55abb3fad52595c92efaf6
Dilnath225 <sunerarathnayaka@gmail.com>
2026-03-17 22:48:06 +0530

    updating logger file

--- internal/config/logger.go
+++ internal/config/logger.go
@@ -1,17 +1,33 @@
 package config
 
 import (
-	"log"
+	"os"
 
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 var Logger *zap.Logger
+var sugar *zap.SugaredLogger
 
 func InitLogger() {
-	var err error
-	Logger, err = zap.NewProduction()
-	if err != nil {
-		log.Fatalf("Failed to initialize logger: %v", err)
+	env := os.Getenv("APP_ENV")
+
+	var cfg zap.Config
+	if env == "production" {
+		cfg = zap.NewProductionConfig()
+
+	} else {
+		cfg = zap.NewDevelopmentConfig()
 	}
+
+	cfg.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+
+	var err error
+	Logger, err = cfg.Build()
+	if err != nil {
+		panic("Failed to initialize logger: " + err.Error())
+	}
+
+	sugar = Logger.Sugar()
 }
