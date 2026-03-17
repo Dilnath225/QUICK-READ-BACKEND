2decf4d83b4e87b7e64bd5d4d93cdaf5bfa30cf8
Dilnath225 <sunerarathnayaka@gmail.com>
2026-03-17 22:39:53 +0530

    import relevent modules for the logger file and creating the initialising loger function

--- internal/config/logger.go
+++ internal/config/logger.go
@@ -1,1 +1,17 @@
 package config
+
+import (
+	"log"
+
+	"go.uber.org/zap"
+)
+
+var Logger *zap.Logger
+
+func InitLogger() {
+	var err error
+	Logger, err = zap.NewProduction()
+	if err != nil {
+		log.Fatalf("Failed to initialize logger: %v", err)
+	}
+}
