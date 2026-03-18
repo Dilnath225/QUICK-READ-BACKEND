8cd3ed92057ac013d42b465cfcf9af5eb8b7b5f8
Dilnath225 <sunerarathnayaka@gmail.com>
2026-03-18 22:31:33 +0530

    updated authentication service file

--- services/auth_service.go
+++ services/auth_service.go
@@ -2,18 +2,34 @@ package services
 
 import (
 	"QUICK-READ-BACKEND/models"
+	"errors"
+
+	"QUICK-READ-BACKEND/config"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 type AuthService struct{}
 
 func (s *AuthService) Register(name, email, password, address, role string) error {
+	//  Hash the Password
+	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+
+	//  Create User
+	if role == "driver" {
+		role = "delivery"
+	}
+	user := models.User{
+		Name:     name,
+		Email:    email,
+		Password: string(hashedPassword),
+		Address:  address,
+		Role:     role,
+	}
+
+	if err := config.DB.Create(&user).Error; err != nil {
+		return errors.New("email already exists")
+	}
+
 	return nil
-}
-
-func (s *AuthService) Login(email, password string) (string, *models.User, error) {
-	return "dummy-token", &models.User{}, nil
-}
-
-func (s *AuthService) UpdateProfile(userID uint, nickName, dob, address, phone, email string) (*models.User, error) {
-	return &models.User{}, nil
 }
