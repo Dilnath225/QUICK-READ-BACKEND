41898bdc7b7f7b9a8356668df40fd941c51fea12
Dilnath225 <sunerarathnayaka@gmail.com>
2026-03-18 23:00:18 +0530

    updated authentication services file add login function and authenticate with jwt

--- services/auth_service.go
+++ services/auth_service.go
@@ -3,9 +3,12 @@ package services
 import (
 	"QUICK-READ-BACKEND/models"
 	"errors"
+	"os"
+	"time"
 
 	"QUICK-READ-BACKEND/config"
 
+	"github.com/golang-jwt/jwt/v5"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -32,4 +35,32 @@ func (s *AuthService) Register(name, email, password, address, role string) erro
 	}
 
 	return nil
+}
+
+func (s *AuthService) Login(email, password string) (string, *models.User, error) {
+	// 1. Find User by Email
+	var user models.User
+	if err := config.DB.Where("email = ?", email).First(&user).Error; err != nil {
+		return "", nil, errors.New("invalid email or password")
+	}
+
+	// 2. Check Password
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)); err != nil {
+		return "", nil, errors.New("invalid email or password")
+	}
+
+	// 3. Generate JWT Token
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": user.ID,
+		"role":    user.Role,
+		"exp":     time.Now().Add(time.Hour * 24).Unix(), // Expires in 24 hours
+	})
+
+	// Sign token with a secret key
+	tokenString, err := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+	if err != nil {
+		return "", nil, err
+	}
+
+	return tokenString, &user, nil
 }
