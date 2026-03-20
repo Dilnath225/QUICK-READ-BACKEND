12b2b416aba90d4a5506dfbf1cfab65e4b51aadf
Nimashi-23 <nimashi.20240444@iit.ac.lk>
2026-03-20 09:33:14 +0530

    feat(payment): add service scaffold and Stripe dependencies

---
+++ services/payment_service.go
@@ -0,0 +1,14 @@
+package services
+
+import (
+	"QUICK-READ-SYSTEM/config"
+	"QUICK-READ-SYSTEM/models"
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/stripe/stripe-go/v76"
+	"github.com/stripe/stripe-go/v76/paymentintent"
+)
+
+type PaymentService struct{}
