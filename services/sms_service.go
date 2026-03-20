9d1d1360b41618d542ece7dcfb637ac62974831d
Kushali Bandara <kushi0716bandara@gmail.com>
2026-03-20 09:29:33 +0530

    feat: add GenerateOTP to SMSService

--- services/sms_service.go
+++ services/sms_service.go
@@ -1,3 +1,15 @@
 package services
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
 type SMSService struct{}
+
+// GenerateOTP creates a random 6-digit OTP
+func (s *SMSService) GenerateOTP() string {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	return fmt.Sprintf("%06d", r.Intn(1000000))
+}
