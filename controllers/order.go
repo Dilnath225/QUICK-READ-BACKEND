e86e7af966233d9d10c3984589f11afa28cdf9d5
Dilnath225 <sunerarathnayaka@gmail.com>
2026-03-21 23:36:15 +0530

    updated order file.created  get order history,initialize chheck out functions

--- controllers/order.go
+++ controllers/order.go
@@ -28,3 +28,40 @@ func CreateOrder(c *gin.Context) {
 
 	utils.SuccessResponse(c, "Order created", createdOrder)
 }
+
+func GetOrderHistory(c *gin.Context) {
+	user, _ := c.Get("user")
+	currentUser := user.(models.User)
+
+	orders, err := orderService.GetUserOrders(currentUser.ID)
+	if err != nil {
+		utils.ErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	utils.SuccessResponse(c, "Order history", orders)
+}
+
+// POST /orders/:id/checkout
+func InitializeCheckout(c *gin.Context) {
+	id := c.Param("id")
+	var input struct {
+		PaymentMethod string `json:"payment_method" binding:"required"` // "card", "cod", "koko"
+	}
+
+	if err := c.ShouldBindJSON(&input); err != nil {
+		utils.ErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	order, clientSecret, err := paymentService.InitializePayment(id, input.PaymentMethod)
+	if err != nil {
+		utils.ErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	utils.SuccessResponse(c, "Checkout initialized", gin.H{
+		"order":         order,
+		"client_secret": clientSecret, // For Stripe Frontend
+	})
+}
