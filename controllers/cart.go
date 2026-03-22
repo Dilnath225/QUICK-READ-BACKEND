306908f4a98d53f1968bc9513bd8b9482c5c7dc5
Lishan Dimantha <lishandimantha2020@gmail.com>
2026-03-21 21:42:43 -0700

    Implement endpoint to add item to cart

--- controllers/cart.go
+++ controllers/cart.go
@@ -8,3 +8,13 @@ import (
 
 	"github.com/gin-gonic/gin"
 )
+
+ AddToCart(c *gin.Context) {
+
+	user, _ := c.Get("user")
+	currentUser := user.(models.User)
+
+	var input struct {
+		MedicineID uint `json:"medicine_id" binding:"required"`
+		Quantity   int  `json:"quantity" binding:"required"`
+	}
